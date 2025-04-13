47a6232988552606b3d4c5275d39e57ac513ca4a
Tatertot717 <tatejsmith@bellsouth.net>
2025-04-13 13:27:52 -0400

    added proper links

--- webapp/src/componets/pages/WelcomePage.tsx
+++ webapp/src/componets/pages/WelcomePage.tsx
@@ -1,12 +1,25 @@
 //1st view on figma/ contains the welcome page and all subcomponets
+'use client'
+
+import Link from 'next/link'
 
 const WelcomePage = () => {
+
     return (
         <div>
             <h1>PollsCheck</h1>
             <h4>Create polls and get feedbackfrom your community!</h4>
-            <p><a href="create poll link">Create a Poll</a> | 
-            <a href="find poll link">Find a Poll</a></p>
+            <Link href="/create-poll">
+  				<button type="button">
+    				Create a Poll
+  				</button>
+			</Link>
+    		<hr />
+            <Link href="/search">
+  				<button type="button">
+    				Find a Poll
+  				</button>
+			</Link>
         </div>
     );
 };
