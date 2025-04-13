0f1334d0f300b10673c0df1f731071515aca2511
William Cox <34105wrc@gmail.com>
2025-04-13 19:36:48 -0400

    Welcome page scaling

--- webapp/src/components/pages/WelcomePage.tsx
+++ webapp/src/components/pages/WelcomePage.tsx
@@ -15,33 +15,33 @@ const WelcomePage = () => {
         <div className="flex flex-col items-center justify-center text-white relative mt-20">
           {/* Create A Poll Card */}
           <Link href="/create-poll">
-          <div className="absolute top-20 left-10 bg-neutral-800 p-4 rounded-md shadow-md w-64">
-            <h2 className="text-lg font-semibold mb-2">Create A Poll</h2>
-            <div className="bg-white text-black p-2 rounded-sm mb-2 text-sm">
-              Enter poll question here
+            <div className="hidden md:block absolute top-20 left-10 bg-neutral-800 p-4 rounded-md shadow-md w-64">
+              <h2 className="text-lg font-semibold mb-2">Create A Poll</h2>
+              <div className="bg-white text-black p-2 rounded-sm mb-2 text-sm">
+                Enter poll question here
+              </div>
+              <div className="bg-white text-black p-2 rounded-sm mb-2 text-sm">
+                Option 1
+              </div>
+              <div className="bg-white text-black p-2 rounded-sm text-sm">
+                Option 2
+              </div>
             </div>
-            <div className="bg-white text-black p-2 rounded-sm mb-2 text-sm">
-              Option 1
-            </div>
-            <div className="bg-white text-black p-2 rounded-sm text-sm">
-              Option 2
-            </div>
-          </div>
           </Link>
           {/* Featured Poll Card */}
           <Link href="/polls">
-          <div className="absolute top-20 right-10 bg-neutral-800 p-4 rounded-md shadow-md w-64">
-            <h2 className="text-lg font-semibold mb-2">Featured Poll</h2>
-            <div className="bg-white text-black p-2 rounded-sm mb-2 text-sm">
-              Top Programming Languages?
+            <div className="hidden md:block absolute top-20 right-10 bg-neutral-800 p-4 rounded-md shadow-md w-64">
+              <h2 className="text-lg font-semibold mb-2">Featured Poll</h2>
+              <div className="bg-white text-black p-2 rounded-sm mb-2 text-sm">
+                Top Programming Languages?
+              </div>
+              <div className="bg-white text-black p-2 rounded-sm mb-2 text-sm">
+                HTML/CSS/JS
+              </div>
+              <div className="bg-white text-black p-2 rounded-sm text-sm">
+                Java
+              </div>
             </div>
-            <div className="bg-white text-black p-2 rounded-sm mb-2 text-sm">
-              HTML/CSS/JS
-            </div>
-            <div className="bg-white text-black p-2 rounded-sm text-sm">
-              Java
-            </div>
-          </div>
           </Link>
 
           {/* Center Text */}
